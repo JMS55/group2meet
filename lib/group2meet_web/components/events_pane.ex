6bacb2b9024c49ead4f127da20d14475ecf05625
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-04-08 18:59:11 -0400

    Add deadline stuff

--- lib/group2meet_web/components/events_pane.ex
+++ lib/group2meet_web/components/events_pane.ex
@@ -1,21 +1,89 @@
+alias Group2meetWeb.CoreComponents
+import CoreComponents
+
 defmodule EventsPane do
-  use Phoenix.LiveComponent
+  use Phoenix.LiveView, container: {:div, class: "contents"}
 
   def render(assigns) do
     ~H"""
-    <div class="p-4 rounded-lg bg-white shadow-lg flex flex-col overflow-y-auto">
+    <div class="p-4 rounded-lg bg-white shadow-lg flex flex-col gap-4 overflow-y-auto">
+      <.in_progress_event in_progress_event={@in_progress_event} />
       <.new_event />
       <.planners />
-      <.events />
+      <.events deadlines={@deadlines} />
     </div>
+    """
+  end
+
+  def mount(_params, %{"group_id" => group_id}, socket) do
+    socket = assign(socket, group_id: group_id)
+    socket = assign(socket, in_progress_event: nil)
+    socket = assign(socket, deadlines: Group2meet.App.get_deadlines(group_id))
+
+    {:ok, socket}
+  end
+
+  def handle_event("new_deadline", _, socket) do
+    socket =
+      if socket.assigns.in_progress_event == nil do
+        assign(socket,
+          in_progress_event:
+            {:deadline, %Group2meet.Deadline{} |> Ecto.Changeset.change() |> to_form()}
+        )
+      else
+        socket
+      end
+
+    {:noreply, socket}
+  end
+
+  def handle_event("create_deadline", %{"deadline" => params}, socket) do
+    {:ok, datetime} = NaiveDateTime.from_iso8601(params["datetime"] <> ":00")
+    {:ok, datetime} = DateTime.from_naive(datetime, "America/New_York")
+
+    {:ok, deadline} =
+      Group2meet.App.create_deadline(
+        %{
+          "datetime" => datetime,
+          "title" => params["title"]
+        },
+        socket.assigns.group_id
+      )
+
+    socket = assign(socket, in_progress_event: nil)
+    {:noreply, socket}
+  end
+
+  defp in_progress_event(%{:in_progress_event => {:deadline, form}} = assigns) do
+    assigns = assign(assigns, form: form)
+
+    ~H"""
+    <.event>
+      <p class="mb-4 text-2xl font-bold tracking-tight">New Deadline</p>
+      <.form for={@form} phx-submit="create_deadline" class="flex flex-col">
+        <.input field={@form[:title]} placeholder="Title" type="text" />
+        <.input field={@form[:datetime]} type="datetime-local" />
+        <.button class="mx-auto mt-4">Create</.button>
+      </.form>
+    </.event>
+    """
+  end
+
+  defp in_progress_event(%{:in_progress_event => nil} = assigns) do
+    ~H"""
+
     """
   end
 
   defp new_event(assigns) do
     ~H"""
     <.event extra_classes="flex justify-around">
-      <button phx-click="new_deadline" class="text-xl tracking-tight font-semibold">Deadline</button>
-      <button phx-click="new_meeting" class="text-xl tracking-tight font-semibold">Meeting</button>
+      <button phx-click="new_deadline" class="text-xl tracking-tight">
+        Deadline
+      </button>
+      <button phx-click="new_meeting" class="text-xl tracking-tight">
+        Meeting
+      </button>
     </.event>
     """
   end
@@ -28,7 +96,28 @@ defmodule EventsPane do
 
   defp events(assigns) do
     ~H"""
-
+    <.event :for={deadline <- @deadlines}>
+      <div class="flex justify-between items-center font-mono">
+        <div class="flex gap-2 items-center">
+          <.icon name="hero-exclamation-circle" class="w-6 h-6" />
+          <p class="text-lg font-bold"><%= deadline.title %></p>
+        </div>
+        <div class="flex flex-col items-end gap-2 font-mono">
+          <p class="text-lg">
+            <%= Calendar.strftime(
+              DateTime.shift_zone!(deadline.datetime, "America/New_York"),
+              "%a, %B %d"
+            ) %>
+          </p>
+          <p>
+            <%= Calendar.strftime(
+              DateTime.shift_zone!(deadline.datetime, "America/New_York"),
+              "%-I:%M %p"
+            ) %>
+          </p>
+        </div>
+      </div>
+    </.event>
     """
   end
 
--- lib/group2meet_web/live/group_live.ex
+++ lib/group2meet_web/live/group_live.ex
@@ -4,15 +4,13 @@ defmodule Group2meetWeb.GroupLive do
   def render(assigns) do
     ~H"""
     <div class="grid grid-cols-2 gap-4 h-full p-4">
-      <.live_component module={EventsPane} id="events_pane" group_id={@group_id} />
+      <%= live_render(@socket, EventsPane, session: %{"group_id" => @group_id}, id: "events_pane") %>
       <%= live_render(@socket, ChatPane, session: %{"group_id" => @group_id}, id: "chat_pane") %>
     </div>
     """
   end
 
   def mount(%{"group_id" => group_id}, %{}, socket) do
-    if connected?(socket), do: Group2meetWeb.Endpoint.subscribe(group_id)
-
     socket = assign(socket, :group_id, group_id)
     {:ok, socket}
   end
